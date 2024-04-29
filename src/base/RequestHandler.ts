360712bcdeef807ecf42a4b7237758b4caa5acee
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-04-29 01:15:48 -0500

    Update RequestHandler.ts

--- src/base/RequestHandler.ts
+++ src/base/RequestHandler.ts
@@ -20,18 +20,6 @@ export const sendRequest = async (url: string, method: 'GET' | 'POST', data?: ob
 
     return response.data
   } catch (error: any) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      throw error.response;
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      throw error.request;
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      throw error.message;
-    }
+    throw error
   }
 }
